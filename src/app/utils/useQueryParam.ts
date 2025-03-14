1187f1f8d4ffef7348f8e6f614e23364e02a8a83
Hazel Shih <hazel@25sprout.com>
2025-03-14 16:58:18 +0800

    feat: remove unnecessary logic

--- src/app/utils/useQueryParam.ts
+++ src/app/utils/useQueryParam.ts
@@ -1,11 +1,11 @@
+"use client";
+
 import { useState, useEffect } from "react";
 
 const useQueryParam = (param: string): string | null => {
   const [value, setValue] = useState<string | null>(null);
 
   useEffect(() => {
-    if (typeof window === "undefined") return; // 伺服器端不執行
-
     const params = new URLSearchParams(window.location.search);
     setValue(params.get(param));
   }, [param]);
